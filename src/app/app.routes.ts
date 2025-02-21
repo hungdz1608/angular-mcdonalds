999ed4443c5c9548037ea8c5b2eea8bed1385bf9
HwngLe <hung.lhc04789@sinhvien.hoasen.edu.vn>
2025-02-21 23:04:07 +0700

    feat:web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {MenuComponent} from './pages/menu/menu.component';
-import {CartComponent} from './pages/cart/cart.component';
 import {PromotionComponent} from './pages/promotion/promotion.component';
 import {NewsComponent} from './pages/news/news.component';
 import {MenuDetailsComponent} from './pages/menu-details/menu-details.component';
@@ -17,10 +16,6 @@ routes = [
   {
     path: 'menu',
     component: MenuComponent,
-  },
-  {
-    path: 'cart',
-    component: CartComponent,
   },
   {
     path: 'promotion',
